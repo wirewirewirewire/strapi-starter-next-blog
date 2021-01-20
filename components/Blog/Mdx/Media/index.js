bf9c46a53b0c8e8427c166b1a23ae86c7722615b
Robert Gühne <robert@polyxostudios.de>
2021-01-20 20:05:15 +0100

    feat(Media): improved view for media

--- components/Blog/Mdx/Media/index.js
+++ components/Blog/Mdx/Media/index.js
@@ -2,25 +2,36 @@ import React from "react";
 import styles from "./media.module.scss";
 import Caption from "../Caption";
 import Lightbox from "./Lightbox";
+import { Cloudinary } from "cloudinary-core";
+var cl = new Cloudinary({ cloud_name: "deeoqlpnx", secure: false });
 
 export default function Media(props) {
   const { src } = props;
 
   const filetype = src.split(".").pop();
-  console.log("filetype", filetype);
 
   const isVideo = filetype === "mov" || filetype === "mp4";
   const thumbnail = isVideo;
 
   const rest = src.substring(0, src.lastIndexOf("/") + 1);
   const last = src.substring(src.lastIndexOf("/") + 1, src.length);
+  const nameWithoutFilename = last.substring(0, last.lastIndexOf("."));
   const scope = rest.substring(0, rest.lastIndexOf("/v") + 1);
+  console.log(filetype, src, rest, last, nameWithoutFilename, scope);
 
-  console.log(scope);
   if (isVideo) {
+    const url = cl.video_url(nameWithoutFilename, {
+      format: "mp4",
+    });
+
+    const imageUrl = cl.video_url(nameWithoutFilename, {
+      width: 1600,
+      format: "jpg",
+    });
+
     return (
-      <video autoPlay loop muted className={styles.video}>
-        <source src={src} type="video/mp4" />
+      <video autoPlay loop muted poster={imageUrl} className={styles.video}>
+        <source src={url} type="video/mp4" />
       </video>
     );
   }
